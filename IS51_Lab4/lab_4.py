b324fa16c7fb02710ed554b4651a37e56afec308
Pheng <phengyangster9@gmail.com>
2022-04-28 13:03:21 -0700

    finish pseudocode

---
+++ IS51_Lab4/lab_4.py
@@ -0,0 +1,35 @@
+
+"""
+This program allows a user three tries to guess the correct answer to the question
+question = "what is the capital of California". The answer in "Sacramento".
+
+We first set max-tries = 3. Then we create a loop to iterate three times. For each iteration,
+we ask the user for the answer (user input). Then based on the answer the user gives, we check 
+to see if the user input matches the answer. If so, print "Correct!", then terminate the loop with a
+break statement.
+
+if the user could not guess the correct answer within the max_tries, then print
+"You have used up your allotment of guesses.", then print "The correct answer is California".
+"""
+
+"""
+main
+    question = "What is the capital or California"
+    answer = "California"
+    ask(question, answer)
+
+ask
+    tries = 0
+    loop three times
+        increment tries by 1
+        ask user input()
+        check to see of user input is equal to answer
+            if so, print "Correct" the exit loop
+    if not correct
+        print to the user "You have used up your allotment of guesses."
+        print the correct answer "The correct answer in 'California'"
+
+main
+"""
+
+
